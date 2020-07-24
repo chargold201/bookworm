806e03ac1604b3d3973e501f2e3feaaa2ec3f900
chargold201 <chargold201@gmail.com>
2020-07-24 02:43:28 -0400

    Add sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'sessions#welcome'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+
   resources :comments
   resources :reviews
   resources :books
   resources :genres
-  resources :users
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
