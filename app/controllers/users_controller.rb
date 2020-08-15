79a74d13d9eea53c2490a2f2f8720adc3dcdad42
chargold201 <chargold201@gmail.com>
2020-08-15 17:48:12 -0400

    Refactor signup process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:message] = "Hello, #{user.name}"
       redirect_to reviews_path
     else
-      render :new
+      # flash message depending on what was wrong
+      redirect_to signup_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :books, through: :reviews
 
   validates :email, uniqueness: true, presence: true
+  validates :name, uniqueness: true
 
   has_secure_password
 end
