9d9637d91bdee6dddb7e3f0a67750722ca51b03d
chargold201 <chargold201@gmail.com>
2020-08-15 17:51:14 -0400

    Add flash messages for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-  def welcome
+  def home
 
   end
 
@@ -9,13 +9,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    session[:user_id] = @user.id
-    redirect_to reviews_path
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:message] = "Hello, #{user.name}"
+      redirect_to reviews_path
+    else
+      flash[:danger] = "Incorrect login info, please try again"
+      redirect_to login_path
+    end
   end
 
   def destroy
     session.clear
-    redirect_to '/'
+    redirect_to root_path
   end
 end
