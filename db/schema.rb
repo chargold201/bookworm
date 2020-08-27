4896f3ba840274fe2921b31282c40ae4c643e16c
chargold201 <chargold201@gmail.com>
2020-08-27 13:51:09 -0400

    Add genre column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_194552) do
+ActiveRecord::Schema.define(version: 2020_08_27_174701) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_194552) do
     t.string "isbn"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre"
   end
 
   create_table "comments", force: :cascade do |t|
