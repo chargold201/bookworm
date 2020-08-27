f93f58ddae68f67a4c2349bf25363806b27ed8c1
chargold201 <chargold201@gmail.com>
2020-08-27 14:17:53 -0400

    Add show pages for books and reviews

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,8 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,8 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
   end
+
+  def show
+    @review = Review.find_by(id: params[:id])
+  end
 end
