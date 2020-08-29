6037e0c3398cd982221b88108f0fd3e9f9377b56
chargold201 <chargold201@gmail.com>
2020-08-29 10:18:00 -0400

    Add form to create new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,26 @@ class BooksController < ApplicationController
   def show
     @book = Book.find_by(id: params[:id])
   end
+
+  def new
+    @book = Book.new
+    @genre_options = Genre.all.map { |g| [ g.name, g.id ] }
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      # flash message with errors
+      flash[:danger] = @book.errors.full_messages
+      redirect_to new_book_path
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre_id, :isbn)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :books, through: :reviews
 
   validates :email, uniqueness: true, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true
 
   has_secure_password
 end
