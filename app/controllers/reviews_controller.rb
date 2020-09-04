be3db1e37fbd1b508b94780d4ccbf175a2c6ea24
chargold201 <chargold201@gmail.com>
2020-09-04 12:28:45 -0400

    Add link to delete a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -32,6 +32,11 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    Review.find(params[:id]).destroy
+    redirect_to reviews_path
+  end
+
   def edit
     @review = Review.find(params[:id])
   end
